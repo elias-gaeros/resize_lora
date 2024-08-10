f146c389135cc0afd3c643203d94cd45cf1fdfe4
Gaeros <163143799+elias-gaeros@users.noreply.github.com>
2024-08-10 20:53:37 +0200

    chop_blocks: support SD1.5 LoRA
    Also don't sugar coat exceptions.

--- chop_blocks.py
+++ chop_blocks.py
@@ -23,21 +23,30 @@ def analyze_lora_layers(
         - A list of tuples, each containing a (section, index) pair and a set of associated keys.
         - A set of pass-through keys (non-LoRA layers).
     """
-    RE_LORA_NAME = re.compile(r"lora_unet_((input|middle|output)_blocks?)_(\d+)_.*")
+    RE_LORA_NAME = re.compile(
+        r"lora_unet_((?:input|middle|output|down|mid|up)_blocks?)(?:(?:_(\d+))?_attentions)?_(\d+)_.*"
+    )
 
     pass_through_keys: set[str] = set()
     block2keys: dict[tuple[str, int], set[str]] = defaultdict(set)
 
     for k in sft_fd.keys():
-        m = RE_LORA_NAME.match(k)
+        m = RE_LORA_NAME.fullmatch(k)
         if not m:
             pass_through_keys.add(k)
             continue
-        section, _, idx = m.groups()
-        idx = int(idx)
+        section, idx1, idx2 = m.groups()
+        if idx1 is None:
+            idx = idx2
+        else:
+            idx = f"{idx1}{idx2}"
 
         block2keys[(section, idx)].add(k)
 
+    if not block2keys:
+        raise ValueError(
+            "No UNet layers found in the LoRA checkpoint (Maybe not a SDXL model?)"
+        )
     block2keys_sorted = sorted(block2keys.items())
     return block2keys_sorted, pass_through_keys
 
@@ -58,15 +67,20 @@ def print_block_layout(
         for i, ((section, idx), v) in enumerate(block2keys):
             logger.info(f"\t[{i:>2d}] {section:>13}.{idx} layers={len(v):<3}")
         section2shortname = {
+            # SDXL names:
             "input_blocks": "INP",
             "middle_block": "MID",
             "output_blocks": "OUT",
+            # SD1 names
+            "down_blocks": "INP",
+            "mid_block": "MID",
+            "up_blocks": "OUT",
         }
         vector_string = ",".join(
-            f"{section2shortname[section]}{idx:02d}" for (section, idx), _ in block2keys
+            f"{section2shortname[section]}{idx:>02}" for (section, idx), _ in block2keys
         )
         logger.info(f'Vector string format: "1,{vector_string}"')
-        vector_string = ",".join('0'*len(block2keys))
+        vector_string = ",".join("0" * len(block2keys))
         logger.info(f'Example (drops all blocks): "1,{vector_string}"')
     else:
         for i, (((section, idx), v), weight) in enumerate(zip(block2keys, weights)):
@@ -159,38 +173,36 @@ def main() -> None:
         help="Increase verbosity (can be repeated)",
     )
     parser.add_argument(
-        "-q", "--quiet",         action="count",
-        default=0, help="Suppress all output except errors"
+        "-q",
+        "--quiet",
+        action="count",
+        default=0,
+        help="Suppress all output except errors",
     )
     args = parser.parse_args()
     setup_logging(args.verbose - args.quiet)
 
-    try:
-        with safe_open(args.input_file, framework="np") as sft_fd:
-            if args.vector_string:
-                # Filter blocks and save the result
-                filtered_state_dict = filter_blocks(sft_fd, args.vector_string)
-                if filtered_state_dict is None:
-                    exit(1)
+    with safe_open(args.input_file, framework="np") as sft_fd:
+        if args.vector_string:
+            # Filter blocks and save the result
+            filtered_state_dict = filter_blocks(sft_fd, args.vector_string)
+            if filtered_state_dict is None:
+                exit(1)
 
-                # Determine output path
-                output_path = args.output or args.input_file.with_stem(
-                    f"{args.input_file.stem}-chop"
-                )
+            # Determine output path
+            output_path = args.output or args.input_file.with_stem(
+                f"{args.input_file.stem}-chop"
+            )
 
-                metadata = sft_fd.metadata()
-                metadata["block_vector_string"] = args.vector_string
-                save_file(filtered_state_dict, output_path, metadata=metadata)
-                logging.info(f"Filtered LoRA saved to {output_path}")
-            else:
-                # Analyze LoRA layers
-                block2keys, pass_through_keys = analyze_lora_layers(sft_fd)
-                print_block_layout(block2keys)
-                logging.info(f"Pass through layers: {len(pass_through_keys)}")
-
-    except Exception as e:
-        logging.error(f"An error occurred: {str(e)}")
-        exit(1)
+            metadata = sft_fd.metadata()
+            metadata["block_vector_string"] = args.vector_string
+            save_file(filtered_state_dict, output_path, metadata=metadata)
+            logging.info(f"Filtered LoRA saved to {output_path}")
+        else:
+            # Analyze LoRA layers
+            block2keys, pass_through_keys = analyze_lora_layers(sft_fd)
+            print_block_layout(block2keys)
+            logging.info(f"Pass through layers: {len(pass_through_keys)}")
 
 
 if __name__ == "__main__":
